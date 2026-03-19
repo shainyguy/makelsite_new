0032fa5b4c5438a172cfdc7ab67644b8afccb2d9
shainyguy <shainyguy@gmail.com>
2026-03-19 13:45:32 +0300

    cikj

--- src/pages/Privacy.jsx
+++
@@ -1,9 +0,0 @@
-import PrivacyContent from '../components/PrivacyPolicy.jsx';
-
-export default function Privacy() {
-  return (
-    <div className="py-12">
-      <PrivacyContent />
-    </div>
-  );
-}
